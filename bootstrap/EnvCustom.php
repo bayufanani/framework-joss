6c7b3ac663e55662d863c55c90df07ca32433a03
Bayu Fanani <fanani707@gmail.com>
2021-10-31 21:11:45 +0700

    add env reader

---
+++ bootstrap/EnvCustom.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Bootstrap;
+
+class EnvCustom
+{
+    /**
+     * lokasi file env yang akan di load
+     * 
+     * @var string
+     */
+    protected $file;
+
+    public function __construct($file)
+    {
+        if (!file_exists($file)) {
+            throw new \Exception("File {$file} tidak ditemukan");
+        }
+
+        $this->file = $file;
+    }
+
+    public function load()
+    {
+        $env = file_get_contents($this->file);
+        $env = explode("\n", $env);
+
+        foreach ($env as $line) {
+            $line = trim($line);
+            if (empty($line)) continue;
+            if (strpos($line, '#') === 0) continue;
+
+            $baris = explode('=', $line);
+            $baris[1] = trim($baris[1]);
+            putenv("{$baris[0]}={$baris[1]}");
+            $_ENV[$baris[0]] = $baris[1];
+        }
+    }
+}
